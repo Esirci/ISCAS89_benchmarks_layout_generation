e536ae6033a9dab006c1666697460760597163a9
fne <fatmanuresirci@gmail.com>
2022-12-09 22:23:38 +0300

    Update top_ISCAS89.v
    call s27 module

--- src/top_ISCAS89.v
+++ src/top_ISCAS89.v
@@ -5,7 +5,7 @@ module top_ISCAS89(
 
 
 
-module s27(.G17(io_out[0]), .VDD(io_in[6]), .GND(io_in[5]), .CK(io_in[0]), 
+s27 IC1(.G17(io_out[0]), .VDD(io_in[6]), .GND(io_in[5]), .CK(io_in[0]), 
 		.G0(io_in[1]), .G1(io_in[2]), .G2(io_in[3]), .G3(io_in[4]));
 
 
